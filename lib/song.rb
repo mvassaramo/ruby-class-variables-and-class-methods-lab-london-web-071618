0abac3df83217a35a95869d7aa584639d80b35f7
Maduri Vassaramo <maduri.vassaramo@hotmail.co.uk>
2018-07-19 10:31:16 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -35,7 +35,7 @@ class Song
     genre_count_hash = {}
     @@genres.keys.each do |genre|
       if genre_count_hash.include?(genre)
-      #   genre_count_hash[genre] << +1 
+      #   genre_count_hash[genre] += 1 
       # else genre_count_hash[genre] = 1
       # end 
     end
