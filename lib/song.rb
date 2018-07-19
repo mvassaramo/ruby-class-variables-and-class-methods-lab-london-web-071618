8b31d121b7857d0b0853b69821762383925c1698
Maduri Vassaramo <maduri.vassaramo@hotmail.co.uk>
2018-07-19 10:16:13 +0000

    automatically backed up by learn

---
+++ lib/song.rb
@@ -0,0 +1,41 @@
+require 'pry'
+
+class Song 
+  
+  attr_accessor :name, :artist, :genre
+  
+  @@count = 0 #count of num of songs created
+  @@all = [] #contain all info for each instance to use to get artists and genres, num of songs of each genre.
+  
+  @@genres = []
+  @@artists = []
+  
+  def initialize(name, artist, genre)
+    @name = name
+    @artist = artist
+    @genre = genre
+    @@count += 1
+    @@genres << genre
+    @@artists << artist 
+  end 
+  
+  def self.count 
+    @@count 
+  end 
+  
+  def self.genres 
+    @@genres.uniq
+  end 
+  
+  def self.artists 
+    @@artists.uniq 
+  end 
+  
+  def self.genre_count 
+    genre_count_hash = {}
+    @@genres.each do |genre|
+      binding.pry 
+    end
+  end
+     
+end 
